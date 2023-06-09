5a70bdfaaa6307ffe1e943235c53c88326011f63
matibe95 <matibel2005@gmail.com>
2023-06-09 13:34:41 -0300

    agregar colores y fuente en tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
-    "./src/**/*.{html,ts}",
+    './src/**/*.{html,ts}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-button': '#2F2E2E',
+        'primary-background': '#1A1A1A',
+        'primary-accent': '#ED6E1E'
+      },
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.mono]
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
-
