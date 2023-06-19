dbff00118502263da3be0f3d0fae2e486431a3a3
matibe95 <matibel2005@gmail.com>
2023-06-19 15:07:51 -0300

    make all color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,27 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {
-        'primary-button': '#2F2E2E',
-        'primary-background': '#1A1A1A',
-        'primary-accent': '#ED6E1E'
+      textColor: {
+        skin: {
+          base: 'var(--color-text-base)',
+          muted: 'var(--color-text-muted)',
+          inverted: 'var(--color-text-inverted)'
+        }
       },
+      backgroundColor: {
+        skin: {
+          fill: 'var(--color-fill)',
+          'button-accent': 'var(--color-button-accent)',
+          'button-accent-hover': 'var(--color-button-accent-hover)',
+          'button-muted': 'var(--color-button-muted)'
+        }
+      },
+      // colors: {
+
+      // 'primary-button': '#2F2E2E',
+      // 'primary-background': '#1A1A1A',
+      // 'primary-accent': '#ED6E1E'
+      // },
       fontFamily: {
         sans: ['Inter', ...defaultTheme.fontFamily.mono]
       }
