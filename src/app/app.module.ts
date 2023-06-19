9e37d5e52961dba5ab8cc1241b6523e71ea4796c
matibe95 <matibel2005@gmail.com>
2023-06-19 15:09:59 -0300

    import all modules and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NavBarComponent } from './landing-page/nav-bar/nav-bar.component';
 import { FooterComponent } from './footer/footer.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { ToggleThemeButtonComponent } from './landing-page/toggle-theme-button/toggle-theme-button.component';
+import { InfoItemComponent } from './landing-page/info-item/info-item.component';
+import {MatIconModule} from '@angular/material/icon';
+import { HttpClientModule } from "@angular/common/http";
+import { MainComponent } from './landing-page/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     FooterComponent,
+    LandingPageComponent,
+    ToggleThemeButtonComponent,
+    InfoItemComponent,
+    MainComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
