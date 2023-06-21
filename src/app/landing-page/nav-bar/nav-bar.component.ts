87263c5f70d2254519b8c89eebcda37a70a03c78
matibe95 <matibel2005@gmail.com>
2023-06-21 16:27:01 -0300

    add open and close modal logic.

--- src/app/landing-page/nav-bar/nav-bar.component.ts
+++ src/app/landing-page/nav-bar/nav-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwitchService } from 'src/app/services/switch.service';
 import { URL_LOGOS } from '../../constants/logoUrl';
 
 @Component({
@@ -9,9 +10,22 @@ import { URL_LOGOS } from '../../constants/logoUrl';
 export class NavBarComponent {
   theme: string = localStorage.getItem('user-theme')!
   logoUrl: string = URL_LOGOS[this.theme]
-  
+  modalSwitch:boolean = true
+
+  constructor(private modalSS: SwitchService){}
+
   updateLogo(){
     this.theme = localStorage.getItem('user-theme')!
     this.logoUrl = URL_LOGOS[this.theme]
+  }
+
+  ngOnInit(){
+    this.modalSS.$modal.subscribe((value)=> {
+      this.modalSwitch = value
+    })
+  }
+
+  openModal(){
+    this.modalSwitch = true
   }
 }
