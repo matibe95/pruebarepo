f3a031cacaab96118ce0a7e93477e904e798dcab
matibe95 <matibel2005@gmail.com>
2023-06-21 16:26:17 -0300

    making main component and use registerIcons func.

--- src/app/landing-page/main/main.component.ts
+++ src/app/landing-page/main/main.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Landing_Icons } from 'src/app/constants/icons';
+import { IconService } from 'src/app/services/Icon.service';
 
 @Component({
   selector: 'app-main',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent {
+  title = 'La App Para Tu'
+  coloredTitle = 'Intercambio Cultural'
+  description = 'Origins, una nueva aplicación que viene para innovar la forma de conocer personas de otros paises e intercambiar conocimientos y culturas.'
+  buttonContent = 'Crear Cuenta'
 
+  constructor(private iconService: IconService){
+    this.iconService.registerIcons(Landing_Icons, 'landing_icons')
+  }
 }
