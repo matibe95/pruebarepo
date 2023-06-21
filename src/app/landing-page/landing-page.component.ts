8c1d608e9a300252b0b591809b7d11e21c988e8d
matibe95 <matibel2005@gmail.com>
2023-06-21 16:24:15 -0300

    use the imported const

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { InfoItem } from '../models/info-item.model';
-// import  from 'src/assets/'
+import { INFO_ITEMS_LIST } from '../constants/infoItem';
 
 @Component({
   selector: 'app-landing-page',
@@ -9,21 +9,5 @@ import { InfoItem } from '../models/info-item.model';
 })
 
 export class LandingPageComponent {
-  infoItems: Array<InfoItem> = [
-    {
-      svgIcon: 'info_message_icon',
-      title: 'Intercambia Culturas',
-      description: 'Intercambia culturas teniendo un chat directo con personas de otros paises.'
-    },
-    {
-      svgIcon: 'info_post_icon',
-      title: 'Posts Multimedia',
-      description: 'Publica todo tu contenido multimedia  a la plataforma directo desde la App.'
-    },
-    {
-      svgIcon: 'info_events_icon',
-      title: 'Eventos Actuales',
-      description: 'Enterate de todos los eventos que hay en tu zona para asistir con tus amigos.'
-    }
-  ]
+  infoItems: Array<InfoItem> = INFO_ITEMS_LIST 
 }
