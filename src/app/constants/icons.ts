b58be850e8430cee7745f912dbad7017d82fc954
matibe95 <matibel2005@gmail.com>
2023-06-21 16:23:05 -0300

    add more enums

---
+++ src/app/constants/icons.ts
@@ -0,0 +1,14 @@
+export enum InfoItemsIcons {
+  info_message_icon,
+  info_events_icon,
+  info_post_icon,
+  // angular = 'angular_logo',
+}
+
+export enum Landing_Icons {
+  mobile_landing_background
+}
+
+export enum Modal_Account_Icons{
+  cross
+}
