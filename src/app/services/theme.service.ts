0d2590d7869b52bf966edac490a09fab5c5e7e4b
matibe95 <matibel2005@gmail.com>
2023-06-21 23:45:01 -0300

    add class to body instead to root

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -14,20 +14,20 @@ export class ThemeService{
 
   initTheme(){
     this.getColorTheme()
-    // this.renderer.addClass(document.body, this.colorTheme)
-    document.documentElement.style.setProperty('color-scheme', this.colorTheme.split('-')[0])
+    this.renderer.addClass(document.body, this.colorTheme)
+    // document.documentElement.style.setProperty('color-scheme', this.colorTheme.split('-')[0])
   }
 
   update(theme: 'dark-mode' | 'light-mode') {
     this.setColorTheme(theme)
 
-    // const previousColorTheme = 
-    //   theme === 'dark-mode' 
-    //     ? 'light-mode' 
-    //     : 'dark-mode'
-    // this.renderer.removeClass(document.body, previousColorTheme)
-    // this.renderer.addClass(document.body, theme)
-    document.documentElement.style.setProperty('color-scheme', theme.split('-')[0])
+    const previousColorTheme = 
+      theme === 'dark-mode' 
+        ? 'light-mode' 
+        : 'dark-mode'
+    this.renderer.removeClass(document.body, previousColorTheme)
+    this.renderer.addClass(document.body, theme)
+    // document.documentElement.style.setProperty('color-scheme', theme.split('-')[0])
   }
 
   isDarkMode(){
