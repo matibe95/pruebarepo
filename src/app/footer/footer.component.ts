5232cf8d9ec3de5284badcc013e6e6f2b83fb1d0
matibe95 <matibel2005@gmail.com>
2023-06-22 20:20:54 -0300

    add dynamic icons

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FOOTER_LINKS } from '../constants/footerLinks';
+import { LinkAndIcon } from '../models/footerLinks';
 
 @Component({
   selector: 'app-footer',
@@ -7,4 +9,5 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
   year = new Date().getFullYear()
+  links: LinkAndIcon[] = FOOTER_LINKS
 }
