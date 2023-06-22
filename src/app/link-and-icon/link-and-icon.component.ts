0adca5f2ab7d5723ef020f5ad34417ceff362bce
matibe95 <matibel2005@gmail.com>
2023-06-22 20:22:29 -0300

    make link-and-icons component

---
+++ src/app/link-and-icon/link-and-icon.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { Footer_Icons } from '../constants/icons';
+import { LinkAndIcon } from '../models/footerLinks';
+import { IconService } from '../services/Icon.service';
+
+@Component({
+  selector: 'link-and-icon',
+  templateUrl: './link-and-icon.component.html',
+  styleUrls: ['./link-and-icon.component.css']
+})
+export class LinkAndIconComponent {
+  @Input() link!: LinkAndIcon
+
+  constructor(private iconService: IconService){
+    this.iconService.registerIcons(Footer_Icons, 'landing_icons')
+  }
+}
