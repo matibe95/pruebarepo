265239ba042c41ffc20b1c70bbdb7985b5d15e27
matibe95 <matibel2005@gmail.com>
2023-06-21 22:23:14 -0300

    Open modal with button and pass props to modal

--- src/app/landing-page/main/main.component.ts
+++ src/app/landing-page/main/main.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Landing_Icons } from 'src/app/constants/icons';
 import { IconService } from 'src/app/services/Icon.service';
+import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
   selector: 'app-main',
@@ -13,7 +14,15 @@ export class MainComponent {
   description = 'Origins, una nueva aplicación que viene para innovar la forma de conocer personas de otros paises e intercambiar conocimientos y culturas.'
   buttonContent = 'Crear Cuenta'
 
-  constructor(private iconService: IconService){
+  constructor(private iconService: IconService, private modalSS: ModalService){
     this.iconService.registerIcons(Landing_Icons, 'landing_icons')
+  }
+
+  openModal(){
+    this.modalSS.$modal.emit({ 
+      originalTitle: '¿Primera vez? Unete ¡Ya!', 
+      coloredWord: 'Ya', 
+      state: true
+    })
   }
 }
