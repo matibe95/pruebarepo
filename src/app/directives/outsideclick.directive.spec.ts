91a65b3277bfdd9e764f09e70aebacca62cebbdb
matibe95 <matibel2005@gmail.com>
2023-06-24 13:06:30 -0300

    delete directive

--- src/app/directives/outsideclick.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { OutsideclickDirective } from './outsideclick.directive';
-
-describe('OutsideclickDirective', () => {
-  it('should create an instance', () => {
-    const directive = new OutsideclickDirective();
-    expect(directive).toBeTruthy();
-  });
-});
