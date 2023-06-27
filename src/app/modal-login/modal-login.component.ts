ebee68af9011dcafc43511659f523f9761a4d828
matibe95 <matibel2005@gmail.com>
2023-06-27 18:46:37 -0300

    delete some keyboard actions. Add name prop to MODAL_STEPS const

--- src/app/modal-login/modal-login.component.ts
+++ src/app/modal-login/modal-login.component.ts
@@ -9,40 +9,35 @@ import { UsuarioService } from '../services/usuario.service';
 @Component({
   selector: 'modal-login',
   templateUrl: './modal-login.component.html',
-  styleUrls: ['./modal-login.component.css']
+  styleUrls: ['./modal-login.component.css'],
 })
-export class ModalLoginComponent {  
-  @Input() modal!: Modal_Account
-  
-  @HostListener('document:keydown', ['$event']) onKeydownHandler(event: KeyboardEvent) {
-    if (event.key === "Escape") this.closeModal()
-    if (event.key === "ArrowRight"){
-      if (this.step > 0) this.updateStep(true)
-    }
-    if (event.key === "ArrowLeft") this.updateStep(false)
-    if (event.key === "Enter"){
-      event.preventDefault()
-      if (this.step > 0) this.updateStep(true)
-    }
+export class ModalLoginComponent {
+  @Input() modal!: Modal_Account;
+
+  @HostListener('document:keydown', ['$event']) onKeydownHandler(
+    event: KeyboardEvent
+  ) {
+    if (event.key === 'Escape') this.closeModal();
   }
 
   constructor(
-    private modalSS: ModalService, 
-    private iconService: IconService, 
+    private modalSS: ModalService,
+    private iconService: IconService,
     private renderer: Renderer2,
     private formBuilder: FormBuilder,
-    private userService: UsuarioService,
-  ){
-      this.renderer.listen('window', 'click', (e: any) => {
-        if (e.target.id === 'modal_container'){
-          this.closeModal()
-        }
-      });
-      this.iconService.registerIcons(Modal_Account_Icons, 'landing_icons')
+    private userService: UsuarioService
+  ) {
+    this.renderer.listen('window', 'click', (e: any) => {
+      if (e.target.id === 'modal_container') {
+        this.closeModal();
+      }
+    });
+    this.iconService.registerIcons(Modal_Account_Icons, 'landing_icons');
   }
 
   MODAL_STEPS = [
     {
+      name: '',
       step: 0,
       buttonType: 'button',
       buttonText: 'Continuar',
@@ -50,86 +45,102 @@ export class ModalLoginComponent {
       placeholder: '',
     },
     {
+      name: 'name',
       step: 1,
       buttonText: 'Continuar',
       buttonType: 'button',
       inputType: 'text',
-      placeholder: 'Ingrese su nombre completo'
+      placeholder: 'Ingrese su nombre completo',
     },
     {
       step: 2,
+      name: 'email',
       buttonText: 'Continuar',
       buttonType: 'button',
       type: 'email',
-      placeholder: 'Ingrese su email'
+      placeholder: 'Ingrese su email',
     },
     {
       step: 3,
+      name: 'password',
       buttonText: 'Continuar',
       buttonType: 'button',
       inputType: 'password',
-      placeholder: 'Ingrese su contraseña'
+      placeholder: 'Ingrese su contraseña',
     },
     {
       step: 4,
+      name: 'password_confirmed',
       buttonText: 'Finalizar',
       buttonType: 'submit',
       inputType: 'password',
-      placeholder: 'Repita su contraseña'
+      placeholder: 'Repita su contraseña',
     },
-  ]
+  ];
 
-  step: number = 0
-  content: any = this.MODAL_STEPS[this.step]
+  step: number = 0;
+  content: any = this.MODAL_STEPS[this.step];
 
   checkoutForm = this.formBuilder.group({
     name: '',
     email: '',
     password: '',
     password_confirmed: '',
-  })
+  });
 
   onSubmit(): void {
-    this.userService.addData(this.checkoutForm.value)
-      .subscribe((res)=>{
-        console.log(res)
-      })
-  }
-  
-  ngOnInit(){
-    this.fillModalWithInfo()
+    if (this.checkoutForm.value.password_confirmed !== this.checkoutForm.value.password) {
+      this.showEmptyError()
+      return    
+    }
+    this.userService.addData(this.checkoutForm.value).subscribe((res) => {
+      console.log(res);
+    });
   }
 
-  fillModalWithInfo(){
-    this.modal.lastCharacter = this.modal.originalTitle?.substring(this.modal.originalTitle.length - 1)
-    const coloredWord = this.modal.coloredWord!
-    const lastCharacter = this.modal.lastCharacter!
-    this.modal.title = this.modal.originalTitle?.replace(coloredWord + lastCharacter, '')
+  showEmptyError(){
+    console.log('matibe')
   }
-  
-  closeModal(){
+
+  ngOnInit() {
+    this.fillModalWithInfo();
+  }
+
+  fillModalWithInfo() {
+    this.modal.lastCharacter = this.modal.originalTitle?.substring(
+      this.modal.originalTitle.length - 1
+    );
+    const coloredWord = this.modal.coloredWord!;
+    const lastCharacter = this.modal.lastCharacter!;
+    this.modal.title = this.modal.originalTitle?.replace(
+      coloredWord + lastCharacter,
+      ''
+    );
+  }
+
+  closeModal() {
     this.modalSS.$modal.emit({
-      state: false
-    })
+      state: false,
+    });
   }
 
-  updateStep(newStep: any){
+  updateStep(newStep: any) {
     if (newStep) {
-      this.nextStep()
-      this.content = this.MODAL_STEPS[this.step]
+      this.nextStep();
+      this.content = this.MODAL_STEPS[this.step];
       return;
     }
-    this.prevStep()
-    this.content = this.MODAL_STEPS[this.step]
+    this.prevStep();
+    this.content = this.MODAL_STEPS[this.step];
   }
 
-  nextStep(){
+  nextStep() {
     if (this.step + 1 > 4) return;
-    this.step += 1
+    this.step += 1;
   }
 
-  prevStep(){
+  prevStep() {
     if (this.step - 1 < 0) return;
-    this.step -= 1
+    this.step -= 1;
   }
 }
