d74995125293a119e658208d5438a9aa0db39f3a
matibe95 <matibel2005@gmail.com>
2023-06-27 18:48:41 -0300

    Add actions to some keys pressed. Show errors when the input its empty

--- src/app/modal-login/modal-step/modal-step.component.ts
+++ src/app/modal-login/modal-step/modal-step.component.ts
@@ -1,17 +1,57 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, ViewChild, ElementRef, HostListener, Input, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormGroupDirective } from '@angular/forms';
+import { trigger, animate,transition, style } from '@angular/animations'
+
+const enterTransition = transition(':enter', [
+  style({ 
+    opacity:0 
+  }),
+  animate('500ms ease-in', style({opacity: 1}))
+])
+const colorRed = trigger('fadeIn', [enterTransition])
 
 @Component({
   selector: 'modal-step',
   templateUrl: './modal-step.component.html',
-  styleUrls: ['./modal-step.component.css']
+  styleUrls: ['./modal-step.component.css'],
+  animations: [colorRed]
 })
 export class ModalStepComponent {
   @Input() content:any
   @Input() currentStep!: number
   @Output() changeStepEvent = new EventEmitter<boolean>();
   buttonType: string = 'button'
+  form!: FormGroup
+  error: boolean = false
+
+  @HostListener ('document:keydown', ['$event']) onKeydownHandler(event: KeyboardEvent) {
+    
+    if (event.key === "ArrowRight"){
+      if (this.content.step > 0) this.updateStep()
+    }
+    if (event.key === "ArrowLeft") this.changeStepEvent.emit(false);
+
+    if (event.key === "Enter"){
+      if (this.content.step > 0) this.updateStep()
+    }
+  }
+
+  @ViewChild('input_info') input!: ElementRef; 
+  constructor (private rootFormGroup: FormGroupDirective){}
+
+  showInputError(){
+    this.error = true
+    setTimeout(()=> {
+      this.error = false
+    }, 600)
+  }
 
   updateStep(){
+    if (this.input){
+      if (this.input.nativeElement.value === '') {
+        return this.showInputError()
+      }
+    }
     if (this.content.step === 4){
       this.buttonType = 'submit'
     }
@@ -22,5 +62,6 @@ export class ModalStepComponent {
   }
 
   ngOnInit(){
+    this.form = this.rootFormGroup.control 
   }
 }
