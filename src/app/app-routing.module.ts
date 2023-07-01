7b5d14e9731c0f13f76816ed24d415bde0cd5532
matibe95 <matibel2005@gmail.com>
2023-07-01 00:20:07 -0300

    make routes for modal register and login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LoginModalComponent } from './login-modal/login-modal.component';
+import { RegisterModalComponent } from './register-modal/register-modal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'welcome',
+    component: LandingPageComponent, 
+    children: [
+      {
+        path: 'login', // child route path
+        component: LoginModalComponent, // child route component that the router renders
+      },
+      {
+        path: 'register', // child route path
+        component: RegisterModalComponent, // child route component that the router renders
+      },
+    ],
+  },
+  {
+    path: '', redirectTo: 'welcome', pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
