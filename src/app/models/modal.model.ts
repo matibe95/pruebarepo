e63a44db7fc1a4b9084e96a80328bfaec4c52081
matibe95 <matibel2005@gmail.com>
2023-07-01 11:41:23 -0300

    set type to Modaltype

--- src/app/models/modal.model.ts
+++ src/app/models/modal.model.ts
@@ -1,7 +1,10 @@
 export interface Modal_Account {
+  type?: ModalType 
   state: boolean
   originalTitle?: string
   coloredWord?: string
   title?: string
   lastCharacter?: string
 }
+
+type ModalType = 'login' | 'register'
