222a2a0fc27128797f77ce34b9264374532b53b6
matibe95 <matibel2005@gmail.com>
2023-07-02 22:26:42 -0300

    add logic to navigation bar

--- src/app/components/main-navbar/main-navbar.component.ts
+++ src/app/components/main-navbar/main-navbar.component.ts
@@ -3,14 +3,25 @@ import { NavBar_Icons } from 'src/app/constants/icons';
 import { NavBarIcon, NAVBAR_ITEMS } from 'src/app/constants/navbarItems';
 import { IconService } from 'src/app/services/Icon.service';
 
+
 @Component({
   selector: 'main-navbar',
   templateUrl: './main-navbar.component.html',
-  styleUrls: ['./main-navbar.component.css']
+  styleUrls: ['./main-navbar.component.css'],
 })
 export class MainNavbarComponent {
   constructor(private iconService: IconService){
     this.iconService.registerIcons(NavBar_Icons, 'main_icons')
   }
   navbarItems: NavBarIcon[] = NAVBAR_ITEMS
+
+  isUploadMenuActive: boolean = false
+
+  updateStep(newStep: any){
+    console.log(newStep)
+  }
+
+  openUploadPostMenu(){
+    this.isUploadMenuActive = !this.isUploadMenuActive
+  }
 }
