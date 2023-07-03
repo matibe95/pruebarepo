162d520ec12f1f502b7e6cc38072d4565adace0c
matibe95 <matibel2005@gmail.com>
2023-07-02 22:25:34 -0300

    create component create-menu

---
+++ src/app/components/create-menu/create-menu.component.ts
@@ -0,0 +1,32 @@
+import { Component, EventEmitter, Output, Type } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
+
+const enterTransition = transition(':enter', [
+  style({
+    // transform: 'translateX(20%)',
+    opacity:0 
+  }),
+  animate('500ms ease-in-out', style({
+    // transform: 'translateX(0)',
+    opacity: 1
+  }))
+])
+const fadeIn = trigger('fadeIn', [enterTransition])
+
+type MenuType = 'post' | 'event' | 'community'
+
+@Component({
+  selector: 'app-create-menu',
+  templateUrl: './create-menu.component.html',
+  styleUrls: ['./create-menu.component.css'],
+  animations: [fadeIn]
+})
+export class CreateMenuComponent {
+  @Output() changeStepEvent = new EventEmitter<boolean>();
+  menuType!: MenuType
+
+  nextStep(){
+    this.menuType = 'post'
+    this.changeStepEvent.emit(true);
+  }
+}
