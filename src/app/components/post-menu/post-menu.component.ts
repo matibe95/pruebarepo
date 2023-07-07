0e43094ec8a6c93a8f579e8fc5b1dffb5b4f17ad
matibe95 <matipro365@gmail.com>
2023-07-07 19:56:22 -0300

    send post when it is created

--- src/app/components/post-menu/post-menu.component.ts
+++ src/app/components/post-menu/post-menu.component.ts
@@ -1,6 +1,16 @@
 import { Component } from '@angular/core';
 import { PostMenu_Icons } from 'src/app/constants/icons';
 import { IconService } from 'src/app/services/Icon.service';
+// import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, FormGroupDirective, Validators } from '@angular/forms';
+import { PostsService } from 'src/app/services/posts.service';
+class ImageSnippet {
+  pending: boolean = false;
+  status: string = 'init';
+
+  constructor(public src: string, public file: File) {}
+}
+
 
 @Component({
   selector: 'app-post-menu',
@@ -8,20 +18,50 @@ import { IconService } from 'src/app/services/Icon.service';
   styleUrls: ['./post-menu.component.css']
 })
 export class PostMenuComponent {
-  constructor(private iconService: IconService){
+  constructor(
+    private iconService: IconService,
+    private formBuilder: FormBuilder,
+    private postService: PostsService
+  ){
     this.iconService.registerIcons( PostMenu_Icons ,'main_icons')
   }
 
+  form!: FormGroup
   url = './assets/main_icons/plus.svg'
+  checkoutForm = this.formBuilder.group({
+    description: '',
+    imagen: '',
+    // password: '',
+  });
 
-  onSelectFile(ev: any){
-    if (ev.target.files){
+  selectedFile!: File;
+
+  onFileSelected(event: any){
+    this.selectedFile = <File>event.target.files[0]
+    if (event.target.files){
       const reader = new FileReader()
-       reader.readAsDataURL(ev.target.files[0])
+       reader.readAsDataURL(event.target.files[0])
        reader.onload = (event: any) => {
         this.url = event.target.result
        }
     }
+  }
+
+  onSubmit(){
+    const arrayImg = [
+      this.selectedFile.name,
+    ]
+
+    const data = {
+      id_usuario: 1,
+      descripcion: this.checkoutForm.value.description, 
+      imagen: arrayImg
+    }
+
+    this.postService.createPost(data).subscribe((res)=>{
+      window.location.reload()
+      // console.log(res)
+    })
   }
 
   loadPost: boolean = false
