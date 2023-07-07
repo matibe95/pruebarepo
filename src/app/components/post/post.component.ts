90b9e395abfc27cd8135464b5f3e30ea18a4fce0
matibe95 <matipro365@gmail.com>
2023-07-07 19:54:09 -0300

    get user id from post and set it to variable

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Post_Icons } from 'src/app/constants/icons';
 import { IconService } from 'src/app/services/Icon.service';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-post',
@@ -10,11 +11,19 @@ import { IconService } from 'src/app/services/Icon.service';
 export class PostComponent {
   @Input() post!: any
 
-  constructor(private iconService: IconService){
+  constructor(private iconService: IconService, private userService: UsuarioService) {
     this.iconService.registerIcons( Post_Icons ,'main_icons')
   }
 
   showOptions: boolean = true
+  userPost: any
+
+  ngOnInit(){
+    this.post = this.post.post
+    this.userService.getUser(this.post.id_usuario).subscribe((res: any) =>{
+      this.userPost = res[0].nickname
+    })
+  }
 
   onToggleOptions(){
     this.showOptions=!this.showOptions;
