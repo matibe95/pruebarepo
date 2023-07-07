2e9109ce0150e23fb1229a428a951167ca24af5c
matibe95 <matipro365@gmail.com>
2023-07-07 20:06:55 -0300

    make getUser function

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -6,6 +6,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class UsuarioService {
   private registerUserUrl = 'http://localhost:8000/api/v1/oauth/register'
+  private getUserUrl = 'http://localhost:8000/api/v1/user/'
+
 
   constructor(private http: HttpClient) { }
   
@@ -17,5 +19,18 @@ export class UsuarioService {
     };
 
     return this.http.post<any>(this.registerUserUrl, data, httpHeaders);
+  }
+
+  getUser(userId: any){
+    const httpOptions = {
+      headers: new HttpHeaders({ 
+        'Content-Type': 'application/json',
+        'Authorization' : 'Bearer ' + localStorage.getItem("accessToken") 
+      })
+    };
+
+    const getUserUrl = this.getUserUrl + userId  
+
+    return this.http.get(getUserUrl, httpOptions);
   }
 }
