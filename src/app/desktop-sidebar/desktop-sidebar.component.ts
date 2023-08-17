6d7b26e89a3a29f6f613b2b63fa61626288f9a2d
matibe95 <matipro365@gmail.com>
2023-08-17 19:30:29 -0300

    create function to open modal

--- src/app/desktop-sidebar/desktop-sidebar.component.ts
+++ src/app/desktop-sidebar/desktop-sidebar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IconService } from '../services/Icon.service';
 import { Desktop_Sidebar } from '../constants/icons';
+import { ModalService } from '../services/modal.service';
 
 @Component({
   selector: 'desktop-sidebar',
@@ -8,7 +9,12 @@ import { Desktop_Sidebar } from '../constants/icons';
   styleUrls: ['./desktop-sidebar.component.css']
 })
 export class DesktopSidebarComponent {
-  constructor(private iconService: IconService){
+  constructor(private iconService: IconService, private modalSS: ModalService){
     this.iconService.registerIcons(Desktop_Sidebar, 'main_icons')
+  }
+
+
+  openUploadMenu(){
+    this.modalSS.$modal_option.emit({state: true, type:'main'})
   }
 }
