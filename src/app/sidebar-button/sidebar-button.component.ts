2aa55c4d11af61b31b467cbaec5b1a960367c058
matibe95 <matipro365@gmail.com>
2023-08-16 23:40:49 -0300

    create sidebar button component. Also, when the route is the same as the button it gets colored

---
+++ src/app/sidebar-button/sidebar-button.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar-button',
+  templateUrl: './sidebar-button.component.html',
+  styleUrls: ['./sidebar-button.component.css']
+})
+export class SidebarButtonComponent {
+  @Input() icon!: String
+  @Input() route!: String
+  background: String = ''
+
+  constructor(private router: Router ) {}
+
+  ngOnInit(){
+    console.log(this.router.url)
+    if (this.router.url === this.route){
+      this.background = 'bg-button-muted' 
+    }
+  }
+}
