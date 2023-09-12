a1a6635e08871c7ebe3cdf515e9feac12e77e59a
matibe95 <matipro365@gmail.com>
2023-09-12 00:54:37 -0300

    update client credentials, as always...

--- src/app/constants/clientCredentials.ts
+++ src/app/constants/clientCredentials.ts
@@ -1,2 +1,2 @@
-export const CLIENT_ID = 2
-export const CLIENT_SECRET = 'GzNvGzMtZkZKScbwlolhspZoWBXQ9FyG36wiU2Z2'
+export const CLIENT_ID = 102
+export const CLIENT_SECRET = 'VBSWFP5FQCbrjoF0BsY9g8XL5jCzwqrNPQFGoMjx'
