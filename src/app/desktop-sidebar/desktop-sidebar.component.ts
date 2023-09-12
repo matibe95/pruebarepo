75df8ce6a91b7b052d0ef69654ff19f75864b078
matibe95 <matipro365@gmail.com>
2023-09-12 00:55:21 -0300

    add function to navigate to home

--- src/app/desktop-sidebar/desktop-sidebar.component.ts
+++ src/app/desktop-sidebar/desktop-sidebar.component.ts
@@ -2,19 +2,21 @@ import { Component } from '@angular/core';
 import { IconService } from '../services/Icon.service';
 import { Desktop_Sidebar } from '../constants/icons';
 import { ModalService } from '../services/modal.service';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'desktop-sidebar',
   templateUrl: './desktop-sidebar.component.html',
   styleUrls: ['./desktop-sidebar.component.css']
 })
 export class DesktopSidebarComponent {
-  constructor(private iconService: IconService, private modalSS: ModalService){
+  constructor(private iconService: IconService, private modalSS: ModalService, private router: Router){
     this.iconService.registerIcons(Desktop_Sidebar, 'main_icons')
   }
 
-
   openUploadMenu(){
     this.modalSS.$modal_option.emit({state: true, type:'main'})
+  }
+  navigateToHome(){
+    this.router.navigateByUrl('/home');
   }
 }
