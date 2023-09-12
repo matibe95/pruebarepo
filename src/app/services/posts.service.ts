b17d7368cb2d110a995953ef39aabdb92ed692fc
matibe95 <matipro365@gmail.com>
2023-09-12 00:56:14 -0300

    add new rutes and functions to service

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -6,21 +6,60 @@ import { Injectable } from '@angular/core';
 })
 export class PostsService {
 
-  private userId = localStorage.getItem('id_user') || 1
-  private listarPost = `http://localhost:8001/api/v1/post/${this.userId}`
-  private crearPost = `http://localhost:8001/api/v1/post`
+  private userId = localStorage.getItem('id_user') || 0
+  private listarPost = `http://localhost:8001/api/v2/post/${this.userId}`
+  private listarFeed = `http://localhost:8001/api/v2/post`
+  private crearPost = `http://localhost:8001/api/v2/post`
+  private likePost = `http://localhost:8001/api/v2/post/like/`
 
   constructor(private http: HttpClient) { }
   
-  listarPosts(){
+  ListarUnPost(){
     const httpHeaders = {
       headers: new HttpHeaders({ 
         'Content-Type': 'application/json',
-        'Authorization' : 'Bearer ' + localStorage.getItem("accessToken") 
+        'Authorization' : 'Bearer ' + localStorage.getItem("accessToken"),
+        'id_usuario' : this.userId
       })
     };
 
     return this.http.get<any>(this.listarPost, httpHeaders);
+  }
+  
+  ListarPostsUsuario(){
+    const httpHeaders = {
+      headers: new HttpHeaders({ 
+        'Content-Type': 'application/json',
+        'Authorization' : 'Bearer ' + localStorage.getItem("accessToken"),
+        'id_usuario' : this.userId
+      })
+    };
+    
+    return this.http.get<any>(this.listarFeed, httpHeaders);
+  }
+
+  ListarFeed(){
+    const httpHeaders = {
+      headers: new HttpHeaders({ 
+        'Content-Type': 'application/json',
+        'Authorization' : 'Bearer ' + localStorage.getItem("accessToken"),
+        'id_usuario' : this.userId
+      })
+    };
+    
+    return this.http.get<any>(this.listarFeed, httpHeaders);
+  }
+  
+  LikePost(body: any){    
+    const httpHeaders = {
+      headers: new HttpHeaders({ 
+        'Content-Type': 'application/json',
+        'Authorization' : 'Bearer ' + localStorage.getItem("accessToken"),
+        'id_usuario' : this.userId
+      })
+    };
+  
+    return this.http.post<any>(this.likePost + body, body, httpHeaders)
   }
 
   createPost(body: any){
