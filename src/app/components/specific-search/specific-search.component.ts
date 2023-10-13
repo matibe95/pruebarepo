89149520c2229b768089d52698240c7ec53eea9f
matibe95 <matipro365@gmail.com>
2023-10-12 22:45:26 -0300

    create new component to search things

---
+++ src/app/components/specific-search/specific-search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-specific-search',
+  templateUrl: './specific-search.component.html',
+  styleUrls: ['./specific-search.component.css']
+})
+export class SpecificSearchComponent {
+
+}
