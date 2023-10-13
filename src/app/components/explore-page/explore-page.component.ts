f85359052024e6e1010f32afdbd378d879203397
matibe95 <matipro365@gmail.com>
2023-10-12 22:47:54 -0300

    onclick change classes to hide post and show other

--- src/app/components/explore-page/explore-page.component.ts
+++ src/app/components/explore-page/explore-page.component.ts
@@ -11,6 +11,9 @@ import { PostsService } from 'src/app/services/posts.service';
 export class ExplorePageComponent {
   postsList!: any[]
   mobile: Boolean = false;
+  showPosts: String = 'hiddenContainer';
+  showOptions: String ='visibleContainer';
+
   @HostListener('window:resize', ['$event'])
   onResize(event: any) {
     if (event.target.innerWidth < 750) {
@@ -23,9 +26,20 @@ export class ExplorePageComponent {
     this.iconService.registerIcons(SearchBar_Icons, 'main_icons')
   }
 
+  onClick(){
+    this.showPosts = 'hiddenContainer'
+    this.showOptions = 'visibleContainer'
+  }
+
   ngOnInit(){
+    if (window.innerWidth < 750) {
+      this.mobile = true
+    } else {
+      this.mobile = false
+    }
     this.postsService.ListarFeed().subscribe((res)=>{
       this.postsList = res.data
+      console.log(this.postsList)
       // this.postsList.reverse()
     })
   }
