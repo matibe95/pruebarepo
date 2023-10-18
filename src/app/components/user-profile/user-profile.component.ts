7f2b1f69460e94fe9a27a8baa88dd93b5be2b95c
matibe95 <matipro365@gmail.com>
2023-10-18 00:02:13 -0300

    modify component and remake it with logic

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { PostsService } from 'src/app/services/posts.service';
 
 @Component({
@@ -9,8 +9,25 @@ import { PostsService } from 'src/app/services/posts.service';
 export class UserProfileComponent {
   constructor(private postsService: PostsService){}
   postsList!: any[]
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    if (event.target.innerWidth < 750) {
+      this.mobile = true
+      return
+    }
+    this.mobile = false
+  }
+  mobile: Boolean = false;
+  showPosts: String = 'visibleContainer';
+  showOptions: String ='hiddenContainer';
+  filterName: String = '';
 
   ngOnInit(){
+    if (window.innerWidth < 750) {
+      this.mobile = true
+    } else {
+      this.mobile = false
+    }
     this.postsService.ListarPostsUsuario().subscribe((res)=>{
       this.postsList = res.data
       // this.postsList.reverse()
