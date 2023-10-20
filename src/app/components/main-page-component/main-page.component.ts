89b756e94f55e5b45fc971b589a8ef5aed4c64ff
matibe95 <matipro365@gmail.com>
2023-10-20 00:10:40 -0300

    import icon service and status service

--- src/app/components/main-page-component/main-page.component.ts
+++ src/app/components/main-page-component/main-page.component.ts
@@ -1,6 +1,10 @@
 import { Component, HostListener } from '@angular/core';
+import { ExplorePage_Icons } from 'src/app/constants/icons';
 import { Post } from 'src/app/models/Post';
+import { SearchFilter } from 'src/app/models/searchfilter.model';
+import { IconService } from 'src/app/services/Icon.service';
 import { PostsService } from 'src/app/services/posts.service';
+import { StatusService } from 'src/app/services/status.service';
 
 @Component({
   selector: 'app-main-page',
@@ -9,7 +13,9 @@ import { PostsService } from 'src/app/services/posts.service';
 })
 export class MainPageComponent {
 
-  constructor(private postsService: PostsService){}
+  constructor(private postsService: PostsService, private statusSS: StatusService, private iconSS: IconService){
+    iconSS.registerIcons(ExplorePage_Icons,'main_icons')
+  }
   @HostListener('window:resize', ['$event'])
   onResize(event: any) {
     if (event.target.innerWidth < 750) {
@@ -18,16 +24,21 @@ export class MainPageComponent {
     }
     this.mobile = false
   }
-
+  feedFilter: SearchFilter = 'post'
   postsList!: any[]
   mobile: Boolean = false;
 
   ngOnInit(){
+    console.log(this.feedFilter)
     if (window.innerWidth < 750) {
       this.mobile = true
     } else {
       this.mobile = false
     }
+
+    this.statusSS.$feedFilter.subscribe((value)=>{
+      this.feedFilter = value
+    })
 
     this.postsService.ListarPostsUsuario().subscribe((res)=>{
       this.postsList = res.data
