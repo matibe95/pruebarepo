0b75be55cd3d6273067726ddaa4b43b76122c3ff
matibe95 <matipro365@gmail.com>
2023-10-19 21:21:02 -0300

    function to navigate to community

--- src/app/components/searched-community-card/searched-community-card.component.ts
+++ src/app/components/searched-community-card/searched-community-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-searched-community-card',
@@ -7,4 +8,12 @@ import { Component, Input } from '@angular/core';
 })
 export class SearchedCommunityCardComponent {
   @Input() community!: any
+
+  constructor(private router: Router){
+
+  }
+
+  openCommunity(){
+    this.router.navigate([`/community/1`]);
+  }
 }
